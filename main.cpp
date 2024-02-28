5bddd8e13c277313f7ea7f9ad186f5f7dccfff0b
Coding-Ocean <eijo524@gmail.com>
2024-02-28 16:52:43 +0900

    comment

--- main.cpp
+++ main.cpp
@@ -3,17 +3,27 @@
 using namespace std;
 
 int main() {
-	set<int> data;
-
-	data.emplace(5);
-	data.emplace(3);
-	data.emplace(2);
-	data.emplace(1);
-	data.emplace(4);
-
-	for (const auto& d : data) {
-		cout << d << " ";
-	}
+	//整数を記憶する平衡2分木
+	set<int> tree;
+	//データ構築
+	tree.emplace(5);
+	tree.emplace(3);
+	tree.emplace(2);
+	tree.emplace(1);
+	tree.emplace(4);
+	//全表示
+	for (const auto& num : tree)
+		cout << num << " ";
+	cout << endl;
+	//探索
+	int num = 2;
+	cout << num << "は";
+	auto it = tree.find(num);
+	if (it != tree.end())
+		cout << "あった";
+	else
+		cout << "なかった";
+	cout << endl;
 
 	system("pause>0");
 }
